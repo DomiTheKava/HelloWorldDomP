3660694555aa5d5acf0b7ba4294018a5cb18dbc6
Dominik <Dominikpenkava>
2024-09-06 12:20:22 -0500

    Initial Commit

---
+++ HelloWorldDomP/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  HelloWorldDomP
+//
+//  Created by Dominik Penkava on 9/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonClicked(_ sender: UIButton) {
+      print("This is a Git tutorial")
+    }
+    
+}
+
